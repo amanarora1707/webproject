91ecccc91036dfa16a8f2938c8a187063182299b
AMAN <amanarora17077@gmail.com>
2021-01-19 18:40:05 +0530

    https

--- public/js/main.js
+++ public/js/main.js
@@ -20,7 +20,7 @@ const getInfo =async (event) =>{
     }
     else{
         try{
-        let url=`http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=c7bd340fabbf2b4ee18bd756a132f1aa`;
+        let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=c7bd340fabbf2b4ee18bd756a132f1aa`;
         const response=await fetch(url);
         const data= await response.json();
         const arrData =[data];
